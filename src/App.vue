abc5be949cb1593ca0f7461bed9500d54fbcdcb2
Abdou <2000abdallah2014@gmail.com>
2022-07-29 20:30:26 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <HelloWorld @NewArrCreator="newarrcreator"/>
+<div class="lists container" v-for="(list, index) in lists" :key="list.number" @click="listExtrcat(index)"> 
+  {{list.number}}
+</div>
+<button @click="selector">Pick someone</button>
+<button @click="saver">Save List</button>
+<h1>{{pickedP}}</h1>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+
+  data(){
+    return{
+      currentlist: null,
+      listName: null,
+    lists:[{
+      number:1,
+      arr: [0,1,2,3,4,5,6]
+    },
+    {
+      number:2,
+      arr: [0,1,2,3,4,5,6]
+    },
+    
+    ],
+      arr:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+      workingArr :[],
+      names:["تجرونة بشرى ","باباوموسى هدى","التومي مارية","بوحديبه غنية","موسى وعلي باية","بابنات مروة","شقبقب فاطمة الزهراء","أويابة سارة","قزريط أسماء","بوسنان سعاد","بوكرموش حفصة","الشيخ صالح فطيمة بنت","حمدي بابا مريم","بوكرموش سيرين","حجاج إسراء","امسيرد ضحى","ليسير بشرى","البدري رحمة","ارفيس فردوس","الشيخ صالح مارية","عوف نسيمة بنت","بازين سلسبيل","بوحديبه خديجة","اويابه بشرى","بابنات صفاء","عطفاوي مامه بنت","باباواسماعيل ملاك","طباخ زينب","قزريط إكرام","موسى وعلي زينب","أوزايد مامة لالة"]
+     ,
+     pickedP: null,
+    }
+  },
+  methods:{
+    listExtrcat(index){
+this.workingArr = this.lists[index].arr;
+this.currentlist = index;
+this.listName = this.lists[index].number;
+console.log(this.workingArr,this.lists[this.currentlist])
+},
+ selector(){
+  
+    if( this.workingArr.length === 0){
+      this.workingArr = this.arr;  
+    }
+  let pickedI = Math.floor(Math.random()*this.workingArr.length);
+  this.pickedP = this.names[this.workingArr[pickedI]]
+  this.workingArr.splice(pickedI,1);
+console.log(this.lists[this.currentlist].arr);
+
+},
+saver(){
+  this.lists.splice(this.currentlist,1,{
+    number: this.listName,
+    arr: this.arr})  ;
+  console.log(this.lists);
+},
+newarrcreator(name){
+  this.lists.push({
+    number : name,
+    arr: this.arr
+  });
+
+  this.workingArr = this.lists[this.lists.length-1].arr;
+this.currentlist = this.lists.length-1;
+ this.listName = this.lists[this.lists.length-1].number;
+   console.log(this.listName, this.lists)
+}
+}
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+ <div class="container">
+<input type="text" v-model="NewlistName">
+<button @click="ArrayCreator"> Create a new array </button>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+data(){
+  return{
+NewlistName : ""
+
+  }
+},
+methods:{
+  ArrayCreator(){
+    this.$emit("NewArrCreator", this.NewlistName);
+  }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
